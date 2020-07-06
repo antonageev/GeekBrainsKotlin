abaec93a87b2be7f5c23e61791f1100235f8bf64
Anton Ageev <antforwork@mail.ru>
2020-07-06 11:48:29 +0300

    added button with click listener

--- app/src/main/java/com/antonageev/geekbrainskotlin/MainActivity.kt
+++ app/src/main/java/com/antonageev/geekbrainskotlin/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.antonageev.geekbrainskotlin
 
 import android.os.Bundle
+import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
@@ -9,6 +10,7 @@ import androidx.lifecycle.ViewModelProviders
 class MainActivity : AppCompatActivity() {
 
     lateinit var viewModel: MainViewModel
+    var helloBtn : Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,8 +21,20 @@ class MainActivity : AppCompatActivity() {
         viewModel.viewState().observe(this, Observer {
             str -> showToast(str)
         })
+
+        initViews()
+
+        initListeners()
     }
 
-    fun showToast(str: String) = Toast.makeText(this, str, Toast.LENGTH_SHORT).show()
+    private fun showToast(str: String?) = Toast.makeText(this, str, Toast.LENGTH_SHORT).show()
+
+    private fun initViews() {
+        helloBtn = findViewById(R.id.helloBtn)
+    }
+
+    private fun initListeners() {
+        helloBtn?.setOnClickListener { v -> showToast(viewModel.viewState().value) }
+    }
 
 }
