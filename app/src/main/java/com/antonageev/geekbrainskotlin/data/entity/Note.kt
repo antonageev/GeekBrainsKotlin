e131c10134f51a4568b3f2f5322952bc14cbca6a
Anton Ageev <antforwork@mail.ru>
2020-07-12 17:16:52 +0300

    Lesson3 completed (as at Lecture)

--- app/src/main/java/com/antonageev/geekbrainskotlin/data/entity/Note.kt
+++ app/src/main/java/com/antonageev/geekbrainskotlin/data/entity/Note.kt
@@ -1,5 +1,35 @@
 package com.antonageev.geekbrainskotlin.data.entity
 
-class Note (val title: String, val text: String, val color: Int ) {
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+import java.util.*
+
+@Parcelize
+data class Note (
+        val id: String = "",
+        val title: String = "",
+        val text: String = "",
+        val color: Color = Color.WHITE,
+        val lastState: Date = Date()) : Parcelable {
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Note
+
+        if (this.id == other.id) return true
+
+        return false
+    }
+
+    enum class Color {
+        WHITE,
+        YELLOW,
+        GREEN,
+        BLUE,
+        RED,
+        VIOLET
+    }
 
 }
--- app/src/main/java/com/antonageev/geekbrainskotlin/ui/main/MainViewState.kt
+++ app/src/main/java/com/antonageev/geekbrainskotlin/ui/main/MainViewState.kt
@@ -2,5 +2,5 @@ package com.antonageev.geekbrainskotlin.ui.main
 
 import com.antonageev.geekbrainskotlin.data.entity.Note
 
-class MainViewState (val notes: List<Note>){
+data class MainViewState (val notes: List<Note>){
 }
